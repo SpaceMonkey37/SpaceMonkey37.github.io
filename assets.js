31e853316c8efd2d487bf579347930a4e6f2360d
SpaceMonkey37 <knut.thomas.rorvik@gmail.com>
2022-02-28 10:54:49 +0100

    commit1
    first commit

---
+++ assets.js
@@ -0,0 +1,16 @@
+let assets;
+
+function Assets(assetType, amount, level, souls, metal, oil, uranium, soulsupgrade, uraniumupgrade, upgrade, harvest, efficiency) {
+  this.assetType = assetType
+  this.amount = amount;
+  this.level = level;
+  this.souls = souls;
+  this.metal = metal;
+  this.oil = oil;
+  this.uranium = uranium;
+  this.soulsupgrade = soulsupgrade;
+  this.uraniumupgrade = uraniumupgrade;
+  this.upgrade = upgrade;
+  this.harvest = harvest;
+  this.efficiency = efficiency;
+}
---
+++ mfdsetup.js
@@ -0,0 +1,24 @@
+let mfdsetup;
+
+function MfdSetup(leftline1, leftline2, leftline3, leftline4, leftline5, leftline6, leftline7, leftline8, rightline1, rightline2, rightline3, rightline4, rightline5, rightline6, rightline7, rightline8, center, leftlinestat, rightlinestat, centerbottom) {
+  this.leftline1 = leftline1;
+  this.leftline2 = leftline2;
+  this.leftline3 = leftline3;
+  this.leftline4 = leftline4;
+  this.leftline5 = leftline5;
+  this.leftline6 = leftline6;
+  this.leftline7 = leftline7;
+  this.leftline8 = leftline8;
+  this.rightline1 = rightline1;
+  this.rightline2 = rightline2;
+  this.rightline3 = rightline3;
+  this.rightline4 = rightline4;
+  this.rightline5 = rightline5;
+  this.rightline6 = rightline6;
+  this.rightline7 = rightline7;
+  this.rightline8 = rightline8;
+  this.center = center;
+  this.leftlinestat = leftlinestat;
+  this.rightlinestat = rightlinestat;
+  this.centerbottom = centerbottom;
+}
