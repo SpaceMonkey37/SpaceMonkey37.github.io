0ec9876c7c37acdabf5cc2f978b3e062c4a41d50
SpaceMonkey37 <knut.thomas.rorvik@gmail.com>
2022-03-21 08:07:24 +0100

    Update1

--- assets.js
+++ assets.js
@@ -1,16 +1,39 @@
-let assets;
-
-function Assets(assetType, amount, level, souls, metal, oil, uranium, soulsupgrade, uraniumupgrade, upgrade, harvest, efficiency) {
-  this.assetType = assetType
-  this.amount = amount;
-  this.level = level;
-  this.souls = souls;
-  this.metal = metal;
-  this.oil = oil;
-  this.uranium = uranium;
-  this.soulsupgrade = soulsupgrade;
-  this.uraniumupgrade = uraniumupgrade;
-  this.upgrade = upgrade;
-  this.harvest = harvest;
-  this.efficiency = efficiency;
+class Assets {
+  constructor (assetType, amount, level, souls, metal, oil, uranium, soulsupgrade, uraniumupgrade, upgrade, harvest, efficiency, variable1, type){
+    this.assetType = assetType;
+    this.amount = amount;
+    this.level = level;
+    this.souls = souls;
+    this.metal = metal;
+    this.oil = oil;
+    this.uranium = uranium;
+    this.soulsupgrade = soulsupgrade;
+    this.uraniumupgrade = uraniumupgrade;
+    this.upgrade = upgrade;
+    this.harvest = harvest;
+    this.efficiency = efficiency;
+    this.variable1 = variable1;
+    this.type = type;
+    this.idle = function() {
+      if (!end) {
+        if(this.amount > 0) {
+          if (this==miner||this==drill) {
+            this.variable1 = metalAmount;
+          }
+          if (this==oilpump||this==oilrig) {
+            this.variable1 = oilAmount;
+          }
+          this.variable1 += (this.efficiency / persek) * this.amount;
+          if (this==miner||this==drill) {
+            metalAmount = this.variable1;
+          }
+          if (this==oilpump||this==oilrig) {
+            oilAmount = this.variable1;
+          }
+          objects();
+          resources();
+        }
+      }
+    };
+  }
 }
